a49b344e6b0b3062d664ed9e41b43f837be48cab
Jeremy Running <jeremy.running@gmail.com>
2026-08-30 18:48:48 -0700

    restore-native-minimized-window

--- LightflowStudio/ApplicationWindowActivation.cs
+++ LightflowStudio/ApplicationWindowActivation.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
 
 namespace LightflowStudio;
 
@@ -25,10 +27,40 @@ internal static class ApplicationWindowActivation
 
 internal sealed class WpfApplicationWindow(Window window, WindowState lastNonMinimizedState) : IApplicationWindow
 {
-    public bool IsMinimized => window.WindowState == WindowState.Minimized;
+    private const int ShowRestore = 9;
+    private const int ShowMaximized = 3;
+    private IntPtr Handle => new WindowInteropHelper(window).Handle;
+
+    public bool IsMinimized => window.WindowState == WindowState.Minimized || IsIconic(Handle);
     public bool IsVisible => window.IsVisible;
-    public void Restore() => window.WindowState = lastNonMinimizedState;
+    public void Restore()
+    {
+        window.WindowState = lastNonMinimizedState;
+        ShowWindow(Handle, lastNonMinimizedState == WindowState.Maximized ? ShowMaximized : ShowRestore);
+    }
     public void Show() => window.Show();
-    public bool Activate() => window.Activate();
+    public bool Activate()
+    {
+        var activated = window.Activate();
+        var foreground = SetForegroundWindow(Handle);
+        if (!activated && !foreground) FlashWindow(Handle, invert: true);
+        return activated || foreground;
+    }
     public bool Focus() => window.Focus();
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool IsIconic(IntPtr windowHandle);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ShowWindow(IntPtr windowHandle, int command);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetForegroundWindow(IntPtr windowHandle);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool FlashWindow(IntPtr windowHandle, [MarshalAs(UnmanagedType.Bool)] bool invert);
 }
